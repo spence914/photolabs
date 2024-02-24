285f1be324a8291e1f78682931498390c46266fc
Spencer Lewis <spence.914@gmail.com>
2024-02-24 15:16:54 -0800

    import and use mock data files instead of hardcoded data for photos and topics

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,59 +3,9 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
+import photos from "mocks/photos";
+
+
 
 const PhotoList = () => {
   const generatePhotoListItems = function(arr) {
@@ -66,7 +16,7 @@ const PhotoList = () => {
   
   return (
     <ul className="photo-list">
-      {generatePhotoListItems(sampleDataForPhotoList)}
+      {generatePhotoListItems(photos)}
     </ul>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,23 +3,9 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
+
+
 
 const TopicList = () => {
   const generateTopicListItems = function(arr) {
@@ -30,7 +16,7 @@ const TopicList = () => {
 
   return (
     <div className="top-nav-bar__topic-list">
-      {generateTopicListItems(sampleDataForTopicList)}
+      {generateTopicListItems(topics)}
     </div>
   );
 };
